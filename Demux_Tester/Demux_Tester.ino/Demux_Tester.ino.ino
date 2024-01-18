a94d7a07caf3fac4d3210568b61c1f3192d29dfd
James Wade <note2jwade@gmail.com>
2024-01-18 16:36:29 -0700

    Shortened the arduino code

--- Demux_Tester/Demux_Tester.ino/Demux_Tester.ino.ino
+++ Demux_Tester/Demux_Tester.ino/Demux_Tester.ino.ino
@@ -2,10 +2,17 @@
 #define ADDRESS2 24
 #define ADDRESS3 26
 
-int state = 0;
+int outputState = 0;
+int inputState = 0;
 unsigned long startMillis;
 unsigned long currentMillis;
 unsigned long period = 1000;
+
+bool address1[8] = {LOW, HIGH, LOW, HIGH, LOW, HIGH, LOW, HIGH};
+bool address2[8] = {LOW, LOW, HIGH, HIGH, LOW, LOW, HIGH, HIGH};
+bool address3[8] = {LOW, LOW, LOW, LOW, HIGH, HIGH, HIGH, HIGH};
+
+void multiplexerFunction();
 
 void setup() {
     Serial.begin(9600);
@@ -22,56 +29,16 @@ void loop() {
     currentMillis = millis();
     if ((currentMillis - startMillis) >= period) {
       startMillis = currentMillis;
-      switch (state) {
-          case 0:
-              digitalWrite(ADDRESS1, LOW);
-              digitalWrite(ADDRESS2, LOW);
-              digitalWrite(ADDRESS3, LOW);
-              state += 1;
-              break;
-          case 1:
-              digitalWrite(ADDRESS1, HIGH);
-              digitalWrite(ADDRESS2, LOW);
-              digitalWrite(ADDRESS3, LOW);
-              state += 1;
-              break;
-          case 2:
-              digitalWrite(ADDRESS1, LOW);
-              digitalWrite(ADDRESS2, HIGH);
-              digitalWrite(ADDRESS3, LOW);
-              state += 1;
-              break;
-          case 3:
-              digitalWrite(ADDRESS1, HIGH);
-              digitalWrite(ADDRESS2, HIGH);
-              digitalWrite(ADDRESS3, LOW);
-              state += 1;
-              break;
-          case 4:
-              digitalWrite(ADDRESS1, LOW);
-              digitalWrite(ADDRESS2, LOW);
-              digitalWrite(ADDRESS3, HIGH);
-              state += 1;
-              break;
-          case 5:
-              digitalWrite(ADDRESS1, HIGH);
-              digitalWrite(ADDRESS2, LOW);
-              digitalWrite(ADDRESS3, HIGH);
-              state += 1;
-              break;
-          case 6:
-              digitalWrite(ADDRESS1, LOW);
-              digitalWrite(ADDRESS2, HIGH);
-              digitalWrite(ADDRESS3, HIGH);
-              state += 1;
-              break;
-          case 7:
-              digitalWrite(ADDRESS1, HIGH);
-              digitalWrite(ADDRESS2, HIGH);
-              digitalWrite(ADDRESS3, HIGH);
-              state = 0;
-              break;
-        }
-        Serial.println(state);
+      digitalWrite(ADDRESS1, address1[outputState]);
+      digitalWrite(ADDRESS2, address2[outputState]);
+      digitalWrite(ADDRESS3, address3[outputState]);
+      outputState += 1;
+      if (outputState == 8) {
+        outputState = 0;
+      }
     }
+}
+
+void multiplexerFunction() {
+  
 }
