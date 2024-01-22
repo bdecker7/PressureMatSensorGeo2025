394ac3fb7ed65eb1b69bde4f37c5e3814fa69369
James Wade <note2jwade@gmail.com>
2024-01-22 12:52:29 -0700

    Started working on multiplexer

--- Demux_Tester/Demux_Tester.ino/Demux_Tester/Demux_Tester.ino
+++ Demux_Tester/Demux_Tester.ino/Demux_Tester/Demux_Tester.ino
@@ -2,6 +2,10 @@
 #define ADDRESS2 24
 #define ADDRESS3 26
 
+#define MUX_S0 32
+#define MUX_S1 34
+#define MUX_S2 36
+
 int outputState = 0;
 int inputState = 0;
 unsigned long startMillisDeMux;
@@ -21,21 +25,30 @@ void setup() {
     pinMode(ADDRESS1, OUTPUT);
     pinMode(ADDRESS2, OUTPUT);
     pinMode(ADDRESS3, OUTPUT);
+    pinMode(MUX_S0, OUTPUT);
+    pinMode(MUX_S1, OUTPUT);
+    pinMode(MUX_S2, OUTPUT);
     digitalWrite(ADDRESS1, LOW);
     digitalWrite(ADDRESS2, LOW);
     digitalWrite(ADDRESS3, LOW);
+    digitalWrite(MUX_S0, HIGH);
+    digitalWrite(MUX_S1, LOW);
+    digitalWrite(MUX_S2, LOW);
+
     startMillisDeMux = millis();
     startMillisMux = millis();
 }
 
+//FIXME I think we need to use smaller resistors since we have two LEDs in series. Either that, or pump up the power. A 200 ohm resistor should be fine, instead of the 260 resistors we have
+
 void loop() {
   currentMillisDeMux = millis();
-  if ((currentMillisDeMux - startMillisDeMux) >= period*8) {
+  if ((currentMillisDeMux - startMillisDeMux) >= period) {
     startMillisDeMux = currentMillisDeMux;
     digitalWrite(ADDRESS1, address1[outputState]);
     digitalWrite(ADDRESS2, address2[outputState]);
     digitalWrite(ADDRESS3, address3[outputState]);
-    multiplexerFunction();
+    //multiplexerFunction();
     outputState += 1;
     if (outputState == 8) {
       outputState = 0;
