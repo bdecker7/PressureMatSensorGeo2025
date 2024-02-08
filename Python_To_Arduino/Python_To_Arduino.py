d300ba909ea7a8d55933711788b36e351b7f490a
Michael Murdock <mm13000@byu.edu>
2024-02-08 16:41:40 -0700

    add: intArrayToString(), stringToIntArray(), display code for debugging arduino output, separate 'on', 'off', 'read', and 'write' commands in python script

--- Python_To_Arduino/Python_To_Arduino.py
+++ Python_To_Arduino/Python_To_Arduino.py
@@ -2,24 +2,55 @@ import serial
 
 import time
 
-serialcomm = serial.Serial('COM4')
 
+def stringToIntList(string: str) -> list[int]:
+    return [int(num) for num in string if num not in ['[',']',',']]
+
+
+# open serial port
+serialcomm = serial.Serial('COM3')
 serialcomm.timeout = 1
 
 while True:
+    # get input command
+    command = input("Enter Input: ").strip()
 
-    i = input("Enter Input: ").strip()
-
-    if i == "Done":
+    if command == "Done":
 
         print('finished')
 
         break
 
-    serialcomm.write(i.encode())
+    if command == "on" or command == "off":
 
-    time.sleep(0.5)
+        serialcomm.write(command.encode())
 
-    print(serialcomm.readline().decode('ascii'))
+        # time.sleep(0.5)
+
+        # val = serialcomm.readline().decode('ascii')
+
+        # print(val)
+
+    if command == "write":
+        
+        serialcomm.write(command.encode())
+    
+    if command == "read":
+
+        string = serialcomm.read_all()
+        print(type(string))
+        print(string)
+        var_decoded = string.decode('ascii')
+        print(type(var_decoded))
+        print(var_decoded)
+
+        listV = stringToIntList(var_decoded)
+        print(type(listV))
+        print(listV)
+
+        # print(serialcomm.read_all().decode('ascii'))
+    
+
+    # print(serialcomm.readline().decode('ascii'))
 
 serialcomm.close()
