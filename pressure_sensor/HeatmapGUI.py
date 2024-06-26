6473c2bddd5392b3279766f240ed47dfb36311d6
WTX\michael.murdock <michael.murdock@wavetronix.com>
2024-06-26 16:10:17 -0600

    Reimplement the heatmap drawing function to directly draw on a Tkinter canvas using a numpy-array image representation of the heatmap which is converted to a Tkinter PhotoImage. Completely eliminate the use of matplotlib which was causing delays (especially when the window size was increased).
    Also use Tkinter's ttk interface (modernized style) rather than tk wherever available to modernize the look of the GUI just a bit.

--- pressure_sensor/HeatmapGUI.py
+++ pressure_sensor/HeatmapGUI.py
@@ -1,9 +1,12 @@
+from email.errors import HeaderMissingRequiredValue
 import tkinter as tk
-from tkinter import messagebox
+import tkinter.ttk as ttk # themed Tkinter
+from tkinter import Tk, messagebox
 from tkinter import filedialog
-from matplotlib import pyplot as plt
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from PIL import Image, ImageTk
+# from matplotlib import pyplot as plt
+# from matplotlib.figure import Figure
+# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
 import time
 import threading
@@ -11,17 +14,18 @@ import os
 
 from DataFromSerial import DataFromSerial
 
-UPDATE_INTERVAL = 0.01 / 2  # Update interval in seconds
+# UPDATE_INTERVAL = 0.01 / 2  # Update interval in seconds
+UPDATE_INTERVAL = 0
 HEATMAP_SIZE = (16, 16)  # Size of the heatmap
 
 class HeatmapGUI:
     """GUI for displaying a heatmap based on data from a serial port."""
 
-    def __init__(self, root, data_getter: DataFromSerial | None = None, vmin=0, vmax=500):
+    def __init__(self, root: Tk, data_getter: DataFromSerial | None = None, vmin=0, vmax=1023):
         if data_getter is None:
             self.debug_mode = True
-            vmin=-1
-            vmax=1
+            # vmin=0
+            # vmax=2
         else:
             self.debug_mode = False
             self.data_getter = data_getter
@@ -33,6 +37,12 @@ class HeatmapGUI:
 
         # Set up the GUI
         self.setup_gui()
+        self.heatmap_canvas.bind("<Configure>", self.on_resize)
+
+        # Choose GUI visual style
+        # call ttk.Style().theme_names() to get a list of available themes
+        s = ttk.Style()
+        s.theme_use('vista')
 
         # Initialize variables
         self.paused = False
@@ -50,16 +60,19 @@ class HeatmapGUI:
     def start(self):
         self.root.mainloop()
 
+    """ GUI Setup (create the visual GUI components) """
+
     def setup_gui(self):
         """Set up the GUI components."""
         self.root.title("GEO Pressure Mat")  # Set the title of the window
         self.root.configure(bg='dimgrey')
 
         # Create a frame for the heatmap
-        self.main_frame = tk.Frame(self.root, bg="dimgrey", border=0, relief=tk.FLAT)
+        # self.main_frame = tk.Frame(self.root, bg="dimgrey", border=0, relief=tk.FLAT)
+        self.main_frame = ttk.Frame(self.root, style='TFrame', border=0, relief=tk.FLAT)
         self.main_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-        self.build_gui_heatmap()
+        self.build_gui_frame_heatmap()
         self.build_gui_sidebar()
 
         # Handle window closing event to properly terminate the program
@@ -72,41 +85,21 @@ class HeatmapGUI:
         window_height = int(screen_height * 2 / 3)
         self.root.geometry(f"{window_width}x{window_height}")
     
-    def build_gui_heatmap(self):
-        # Create a Matplotlib figure and axis
-        self.fig, self.ax = plt.subplots()  # Create a Matplotlib figure and axis
-        self.fig.patch.set_facecolor('dimgrey')  # Set the background color of the figure
-        self.ax.tick_params(axis='x', colors='white')  # Set the x-axis tick color to white
-        self.ax.tick_params(axis='y', colors='white')  # Set the y-axis tick color to white
-        self.plot_frame = tk.Frame(self.main_frame, bg="dimgrey")  # Create a frame for the plot
+    def build_gui_frame_heatmap(self):
+        # Build a canvas for the heatmap, to hold the heatmap canvas and a legend/colorbar for the heatmap
 
-        # Create a Tkinter canvas using the Matplotlib figure
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.main_frame)
-        self.canvas_widget = self.canvas.get_tk_widget()  # Get the Tkinter widget from the canvas
-        self.canvas_widget.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)  # Pack the Tkinter widget
-
-        # Add a colorbar to the heatmap
-        self.colorbar = self.ax.imshow(np.zeros(HEATMAP_SIZE), cmap='jet', interpolation='nearest',
-                      vmin=self.vmin, vmax=self.vmax)
-        self.colorbar.colorbar = self.fig.colorbar(self.colorbar)
-        self.colorbar.colorbar.ax.tick_params(axis='y', colors='white')
-        self.colorbar.colorbar.ax.yaxis.set_tick_params(color='white')
+        # Create a canvas for the heatmap, this is where the heatmap is actually drawn
+        self.heatmap_canvas: tk.Canvas = tk.Canvas(self.main_frame, bg="dimgrey", border=0, relief=tk.FLAT)
+        self.heatmap_canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
     def build_gui_sidebar(self):
         # Create a sidebar frame
-        self.sidebar = tk.Frame(self.main_frame, bg="dimgrey", border=0, relief=tk.FLAT, width=200)
+        self.sidebar = ttk.Frame(self.main_frame, style='TFrame', border=0, relief=tk.FLAT, width=200)
         self.sidebar.pack(side=tk.RIGHT)
 
         # Create a pause button that dyanmically changes size based on the window size
-        self.pause_button = tk.Button(self.sidebar, text="Pause", font=("Arial", 14, "bold"), command=self.toggle_pause,
-                                      bg="dimgrey", fg="white")
+        self.pause_button = ttk.Button(self.sidebar, text="Pause", style='TButton', command=self.toggle_pause, width=10)
         self.pause_button.pack(side=tk.TOP)  # Pack the pause button
-
-        # Set up the click event
-        self.clicked_coords = None
-        self.clicked_value_label = tk.Label(self.sidebar, bg="dimgrey", fg="white")
-        self.clicked_value_label.pack(side=tk.TOP)
-        self.fig.canvas.mpl_connect('button_press_event', self.on_heatmap_click)
 
         # Add space for displaying statistics
         self.build_gui_frame_stats()
@@ -119,63 +112,42 @@ class HeatmapGUI:
 
     def build_gui_frame_stats(self):
         # Create a frame for related stats (inside the sidebar)
-        self.stats_frame = tk.Frame(self.sidebar, bg="dimgrey", border=0, relief=tk.FLAT)
+        self.stats_frame = ttk.Frame(self.sidebar, style='TFrame', border=0, relief=tk.FLAT)
         self.stats_frame.pack(side=tk.TOP)
-        self.data_label = tk.Label(self.stats_frame, text="Stats:", font=("Arial", 14, "bold"), bg="dimgrey", fg="white")
+        self.data_label = ttk.Label(self.stats_frame, text="Stats:", style='TLabel', font=("Arial", 14, "bold"))
         self.data_label.pack()
         self.stats_text_box = tk.Text(self.stats_frame, height=5, width=10, bg="dimgrey", fg="white")
         self.stats_text_box.pack()
 
     def build_gui_frame_record_export(self):
-        self.record_frame = tk.Frame(self.sidebar, bg="dimgrey", border=0, relief=tk.FLAT)
+        self.record_frame = ttk.Frame(self.sidebar, style='TFrame', border=0, relief=tk.FLAT)
         self.record_frame.pack(side=tk.TOP)
-        self.export_label = tk.Label(self.record_frame, text="Save and Export:", font=("Arial", 14, "bold"),
-                                     bg="dimgrey", fg="white")
+        self.export_label = ttk.Label(self.record_frame, text="Save and Export:", style='TLabel')
         self.export_label.pack()
 
         # Create a record button and timer
-        self.record_button = tk.Button(self.record_frame, text="Record Data", command=self.toggle_record,
-                                       bg="dimgrey", fg="white")
+        self.record_button = ttk.Button(self.record_frame, text="Record Data", command=self.toggle_record)
         self.record_button.pack()  # Pack the record button
-        self.record_time_display = tk.Label(self.record_frame, text="Time: 0:00", bg="dimgrey", fg="white")
+        self.record_time_display = ttk.Label(self.record_frame, text="Time: 0:00", style='TLabel')
         self.record_time_display.pack()  # Pack the record time display
 
         # Create an export button
-        self.export_button = tk.Button(self.record_frame, text="Export Data", command=self.export_data,
-                                       bg="dimgrey", fg="white")
+        self.export_button = ttk.Button(self.record_frame, text="Export Data", command=self.export_data)
         self.export_button.pack()  # Pack the export button
     
     def build_gui_frame_zoom(self):
         # Create a frame for the zoom slider (inside the sidebar)
-        self.zoom_frame = tk.Frame(self.sidebar, bg="dimgrey", border=0, relief=tk.FLAT)
+        self.zoom_frame = ttk.Frame(self.sidebar, style='TFrame', border=0, relief=tk.FLAT)
         self.zoom_frame.pack(side=tk.TOP)
-        self.zoom_label = tk.Label(self.zoom_frame, text="Zoom:", font=("Arial", 14, "bold"), bg="dimgrey", fg="white")
+        self.zoom_label = ttk.Label(self.zoom_frame, text="Zoom:", style='TLabel')
         self.zoom_label.pack()
-        self.zoom_slider = tk.Scale(self.zoom_frame, from_=0, to=20, orient=tk.HORIZONTAL, bg="dimgrey", fg="white")
+        self.zoom_slider = ttk.Scale(self.zoom_frame, from_=0, to=20, orient=tk.HORIZONTAL, style='TScale')
         self.zoom_slider.pack()
 
         # Connect the zoom slider to a function that changes the zoom level
         self.zoom_slider.bind("<ButtonRelease-1>", self.change_zoom)
-    
-    def change_zoom(self, event):
-        # Change the size of text in the sidebar based on the zoom level
-        self.zoom_level = int(self.zoom_slider.get())
-        self.font_size = 14 + self.zoom_level
-        self.data_label.config(font=("Arial", self.font_size, "bold"))
-        self.export_label.config(font=("Arial", self.font_size, "bold"))
-        self.zoom_label.config(font=("Arial", self.font_size, "bold"))
-        self.clicked_value_label.config(font=("Arial", self.font_size, "bold"))
-        self.record_button.config(font=("Arial", self.font_size, "bold"))
-        self.export_button.config(font=("Arial", self.font_size, "bold"))
-        self.pause_button.config(font=("Arial", self.font_size, "bold"))
-        self.record_time_display.config(font=("Arial", self.font_size, "bold"))
-        self.stats_text_box.config(font=("Arial", self.font_size, "bold"))
 
-        # Change the size of the text axis labels on the heatmap based on the zoom level
-        self.ax.tick_params(axis='x', labelsize=self.font_size - 2)
-        self.ax.tick_params(axis='y', labelsize=self.font_size - 2)
-        self.colorbar.colorbar.ax.yaxis.set_tick_params(labelsize=self.font_size - 2)
-        self.canvas.draw()
+    """ GUI Update Loop (running in background thread) """
 
     def gui_update_loop(self):
         """Continuously update the GUI with new data."""
@@ -199,50 +171,60 @@ class HeatmapGUI:
                     self.record_time_display.config(text=f"Time: {record_time:.2f}")
 
                 # Update the display
-                self.display_heatmap()
-                self.display_coords()
+                self.draw_heatmap()
                 self.display_stats() 
 
             time.sleep(UPDATE_INTERVAL)
 
-    def display_coords(self):
-        if self.clicked_coords:
-            clicked_value = self.data[self.clicked_coords]
-            self.clicked_value_label.config(text=f"Value at {self.clicked_coords}: {clicked_value:.2f}")
+    def draw_heatmap(self, canvas_resized: bool = False):
+        # Calculate the width and height (in pixels) of each cell in the heatmap
+        rows, cols = self.data.shape
+        cell_width_pixels = int(self.heatmap_canvas.winfo_width() / cols)
+        cell_height_pixels = int(self.heatmap_canvas.winfo_height() / rows)
 
-    def display_heatmap(self):
-        self.ax.clear()
-        self.ax.imshow(self.data, cmap='jet', interpolation='nearest', vmin=self.vmin, vmax=self.vmax)
-        self.canvas.draw()
+        if cell_width_pixels == 0 or cell_height_pixels == 0:
+            return # On the first iteration the canvas size is sometimes 0, so skip drawing in that case to avoid errors
 
-    def toggle_pause(self):
-        # Toggle the pause state of the updates
-        self.paused = not self.paused
-        # Change the text of the button based on the pause state
-        if self.paused:
-            self.pause_button.config(text="Resume")
-        else:
-            self.pause_button.config(text="Pause")
-    
-    def on_heatmap_click(self, event):
-        """Handle click events on the heatmap."""
-        self.clicked_coords = (int(round(event.ydata)), int(round(event.xdata)))
-    
+        # The heatmap starts out as a copy of the data
+        heatmap_image: np.ndarray = self.data.copy()
+
+        # Convert the data into RGB colors based on its value
+        heatmap_image = heatmap_image / (self.vmax - self.vmin) * 255 # Scale the data to 0-255
+        heatmap_image = np.repeat(heatmap_image[:, :, np.newaxis], 3, axis=2) # Create 3 RGB channels
+        heatmap_image[:, :, 0] = np.where(heatmap_image[:, :, 0] <= 127, 0, heatmap_image[:, :, 0]) # red channel: 0's until 127, then linear ramp from 128 to 255
+        heatmap_image[:, :, 1] = np.zeros_like(heatmap_image[:, :, 1]) # set the green channel to 0
+        heatmap_image[:, :, 2] = 255 - 2*np.abs(heatmap_image[:, :, 2] - 127) # blue channel: a triangle function where 0 -> 0 | 127 -> 255 | 255 -> 0
+
+        # heatmap_image[0, 0] = [255, 0, 0] # highlight the top-left corner for debugging
+        
+        heatmap_image = heatmap_image.astype(np.uint8)
+
+        # Scale the data to the size of the heatmap canvas in pixels by repeating the values
+        heatmap_image = np.repeat(heatmap_image, cell_width_pixels, axis=1)
+        heatmap_image = np.repeat(heatmap_image, cell_height_pixels, axis=0)
+        heatmap_image = np.ascontiguousarray(heatmap_image) # make it a C-contiguous array for faster drawing
+
+        # Convert the numpy array to a Tkinter PhotoImage
+        pil_image = Image.fromarray(heatmap_image)
+
+        if not hasattr(self, 'heatmap_photo') or canvas_resized: # first time or when canvas is resized, create the image
+            self.heatmap_photo = ImageTk.PhotoImage(pil_image)
+            self.heatmap_image_id = self.heatmap_canvas.create_image(0, 0, image=self.heatmap_photo, anchor=tk.NW)
+        else: # update the image (updating this way rather than re-creating it is faster and eliminates flickering)
+            self.heatmap_photo.paste(pil_image)
+            self.heatmap_canvas.coords(self.heatmap_image_id, (0, 0))
+
     def get_simulated_data(self):
         """Generate debug (fake) data for the heatmap."""
         data = np.zeros(HEATMAP_SIZE)
         # Generate a heatmap based on a sine wave
         for i in range(HEATMAP_SIZE[0]):
             for j in range(HEATMAP_SIZE[1]):
-                data[i][j] = ( np.sin(2 * np.pi * j / HEATMAP_SIZE[0]) 
-                              * np.sin(np.pi * i / HEATMAP_SIZE[1]) 
-                              * np.sin(time.time()) )
+                data[i][j] = np.abs( np.sin(2 * np.pi * (j + 1/2) / HEATMAP_SIZE[0]) 
+                              * np.sin(np.pi * (i + 1/2) / HEATMAP_SIZE[1]) 
+                              * np.sin(time.time()) ) * 1023
         return data
-    
-    def on_close(self):
-        if messagebox.askokcancel("Quit", "Do you want to quit?"):
-            self.root.quit()
-    
+
     def get_stats(self):
         """Display the mean, standard deviation, min, and max of the heatmap data."""
         self.mean = np.abs(np.mean(self.data))
@@ -258,6 +240,24 @@ class HeatmapGUI:
         self.stats_text_box.insert(tk.END, f"Std: {self.std:.2f}\n")
         self.stats_text_box.insert(tk.END, f"Min: {self.min_val:.2f}\n")
         self.stats_text_box.insert(tk.END, f"Max: {self.max_val:.2f}\n")
+
+    """ Event handlers """
+
+    def toggle_pause(self):
+        # Toggle the pause state of the updates
+        self.paused = not self.paused
+        # Change the text of the button based on the pause state
+        if self.paused:
+            self.pause_button.config(text="Resume")
+        else:
+            self.pause_button.config(text="Pause")
+    
+    def on_close(self):
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            self.root.quit()
+    
+    def on_resize(self, event):
+        self.draw_heatmap(canvas_resized=True)
 
     def toggle_record(self):
         self.record_mode = not self.record_mode
@@ -280,7 +280,16 @@ class HeatmapGUI:
         file_path = os.path.join(folder_path, file_name)
         np.savetxt(file_path, self.recorded_data, delimiter=",")
         messagebox.showinfo("Export Successful", f"Data exported to {file_path}")
-    
+    def change_zoom(self, event):
+        # Change the size of text in the sidebar based on the zoom level
+        self.zoom_level = int(self.zoom_slider.get())
+        self.font_size = 14 + self.zoom_level
+        self.data_label.config(font=("Arial", self.font_size, "bold"))
+        self.export_label.config(font=("Arial", self.font_size, "bold"))
+        self.zoom_label.config(font=("Arial", self.font_size, "bold"))
+        self.record_time_display.config(font=("Arial", self.font_size, "bold"))
+        self.stats_text_box.config(font=("Arial", self.font_size, "bold"))
+
 if __name__ == "__main__":
     root = tk.Tk() # create main Tkinter window
     app = HeatmapGUI(root) # initialize heatmap in main window (in debug mode because no data getter is provided)
